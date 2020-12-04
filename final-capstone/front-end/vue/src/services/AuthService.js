6d9e7ddb2198ea3adac0c7f4cbe5ee0ad3d4aa02
Frank Fella <frank@techelevator.com>
2020-12-04 05:53:56 -0500

    Instructor Push 12/04/2020 05:53:55

--- final-capstone/front-end/vue/src/services/AuthService.js
+++ final-capstone/front-end/vue/src/services/AuthService.js
@@ -1,3 +1,10 @@
+/**************************************************************************************
+* This file contains services used by the login front-end starter code
+*
+* DO NOT MODIFY THIS CODE! YOU MAY RENDER IT UNUSABLE!
+*
+* Consider putting any services required by your application in a different file.
+***************************************************************************************/
 import axios from 'axios';
 
 export default {
