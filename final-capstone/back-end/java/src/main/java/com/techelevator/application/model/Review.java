50a5e114de1c82577cdd9c72b1e65b2423ee91e3
BrendanGlancy <bglance68@gmail.com>
2020-12-05 13:00:33 -0500

    review

--- final-capstone/back-end/java/src/main/java/com/techelevator/application/model/Review.java
+++ final-capstone/back-end/java/src/main/java/com/techelevator/application/model/Review.java
@@ -1,5 +1,67 @@
 package com.techelevator.application.model;
 
-public class Review {
+import java.time.LocalDateTime;
 
+public class Review {
+	// Instance Variables
+	private String id;
+	private String description;
+	private int rating;
+	private LocalDateTime createTime;
+	private long beerId;
+	private long userId;
+	
+
+	/****************************************
+	 * Getters and Setters
+	 *
+	 ***/
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public int getRating() {
+		return rating;
+	}
+	
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	public LocalDateTime getCreateTime() {
+		return createTime;
+	}
+	
+	public void setCreateTime(LocalDateTime createTime) {
+		this.createTime = createTime;
+	}
+	public long getBeerId() {
+		return beerId;
+	}
+	
+	public void setBeerId(long beerId) {
+		this.beerId = beerId;
+	}
+	
+	public long getUserId() {
+		return userId;
+	}
+	
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+	
+	
 }
