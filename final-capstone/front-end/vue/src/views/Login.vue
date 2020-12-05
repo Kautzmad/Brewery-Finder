6ff438281a356d41d1725b69996562ecc4f3253a
Vincent DeMichele <vincedemichele@gmail.com>
2020-12-05 15:57:16 -0500

    style login

--- final-capstone/front-end/vue/src/views/Login.vue
+++ final-capstone/front-end/vue/src/views/Login.vue
@@ -1,48 +1,16 @@
 <template>
-  <div id="login" class="text-center">
-    <form class="form-signin" @submit.prevent="login">
-      <h1 class="h3 mb-3 font-weight-normal">Please Sign In</h1>
-      <div
-        class="alert alert-danger"
-        role="alert"
-        v-if="invalidCredentials"
-      >Invalid username and password!</div>
-      <div
-        class="alert alert-success"
-        role="alert"
-        v-if="this.$route.query.registration"
-      >Thank you for registering, please sign in.</div>
-      <label for="username" class="sr-only">Username</label>
-      <input
-        type="text"
-        id="username"
-        class="form-control"
-        placeholder="Username"
-        v-model="user.username"
-        required
-        autofocus
-      />
-      <label for="password" class="sr-only">Password</label>
-      <input
-        type="password"
-        id="password"
-        class="form-control"
-        placeholder="Password"
-        v-model="user.password"
-        required
-      />
-      <router-link :to="{ name: 'register' }">Need an account?</router-link>
-      <button type="submit">Sign in</button>
-    </form>
-  </div>
+  <login/>
 </template>
 
 <script>
 import authService from "../services/AuthService";
+import Login from "../components/Login"
 
 export default {
-  name: "login",
-  components: {},
+  name: "login-view",
+  components: {
+    Login
+  },
   data() {
     return {
       user: {
@@ -74,3 +42,9 @@ export default {
   }
 };
 </script>
+
+
+
+<style scoped>
+
+</style>
