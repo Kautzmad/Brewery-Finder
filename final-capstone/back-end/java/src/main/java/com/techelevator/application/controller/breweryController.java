866fc23af8f7b8e8e25a43d6752e38758d0fb140
Brandon Eads <beads1997@gmail.com>
2020-12-05 17:08:13 -0500

    Breweries can show up now!

--- final-capstone/back-end/java/src/main/java/com/techelevator/application/controller/breweryController.java
+++ final-capstone/back-end/java/src/main/java/com/techelevator/application/controller/breweryController.java
@@ -3,6 +3,7 @@ package com.techelevator.application.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.techelevator.application.dao.breweryDao;
 import com.techelevator.application.model.Brewery;
 
 @RestController
+@CrossOrigin 
 public class breweryController {
 	
 	@Autowired
--- final-capstone/front-end/vue/src/components/BreweryCard.vue
+++ final-capstone/front-end/vue/src/components/BreweryCard.vue
@@ -1,13 +1,12 @@
 <template>
     <div id="card">
         <!-- <img src="{{brewery.breweryLogoUrl}}"/> -->
-        <h1>{{brewery.name}}</h1>
+        <h1>{{this.brewery.name}}</h1>
     </div>
 </template>
 <script>
 export default {
     props: ["brewery"]
-
 }
 </script>
 <style scoped>
