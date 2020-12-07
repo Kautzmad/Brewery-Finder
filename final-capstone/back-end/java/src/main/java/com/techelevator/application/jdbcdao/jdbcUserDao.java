1945d5d43f3311d7cae20cab8dd124e829bad3fb
John Mullen <johnpatrickmullen88@gmail.com>
2020-12-07 09:33:54 -0500

    minor adds to jdbcUserDao

--- final-capstone/back-end/java/src/main/java/com/techelevator/application/jdbcdao/jdbcUserDao.java
+++ final-capstone/back-end/java/src/main/java/com/techelevator/application/jdbcdao/jdbcUserDao.java
@@ -1,5 +1,20 @@
 package com.techelevator.application.jdbcdao;
 
-public class jdbcUserDao {
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import com.techelevator.application.dao.userDao;
+
+@Component
+public class jdbcUserDao implements userDao{
+
+	private JdbcTemplate jdbcTemplate;
+	
+	public jdbcUserDao(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
 
 }
