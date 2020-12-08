79d5923544351e09c619989241782aec2b7b3efa
Douglas Kautzman <kautzmad63@gmail.com>
2020-12-07 21:59:27 -0500

    updated Brewery database witha few new breweries and created a .csv file

---
+++ final-capstone/back-end/java/database/Breweries.sql
@@ -0,0 +1,62 @@
+BEGIN;
+
+INSERT INTO breweries (name, address, city, zipcode, phone_number, description, brewery_logo_url, hours, user_id, lat, lng)
+VALUES ('16 Lots Brewing','753 Reading Rd','Mason','45040-1303','5134863672','','','',2,-84.3183801,39.3545967);
+
+INSERT INTO breweries (name, address, city, zipcode, phone_number, description, brewery_logo_url, hours, user_id, lat, lng)
+VALUES ('2 Tones Brewing Co.','4539 E Broad St','Whitehall','43213-1308','','','','',2,-82.9118174,40.0030689);
+
+INSERT INTO breweries (name, address, city, zipcode, phone_number, description, brewery_logo_url, hours, user_id, lat, lng)
+VALUES ('4kd Crick Brewery','211 Carpenter Rd','Defiance','43512-1718','4199562863','','','',2,-84.341647,41.296252);
+
+INSERT INTO breweries (name, address, city, zipcode, phone_number, description, brewery_logo_url, hours, user_id, lat, lng)
+VALUES ('Actual Brewing Company, LLC','655 N James Rd','Columbus','43219-1837','6146363825','','','',2,-82.9080986,39.9868523);
+
+INSERT INTO breweries (name, address, city, zipcode, phone_number, description, brewery_logo_url, hours, user_id, lat, lng)
+VALUES ('Akronym Brewing LLC','58 E Market St','Akron','4430','3306208274','','','',2,-81.51645882,41.08512193);
+
+
+INSERT INTO breweries (name, address, city, zipcode, phone_number, description, brewery_logo_url, hours, user_id, lat, lng)
+VALUES ('Alematic Artisan Ales','','Huber Heights','45424-3857','9376122337','','','',2,'','');
+
+
+INSERT INTO breweries (name, address, city, zipcode, phone_number, description, brewery_logo_url, hours, user_id, lat, lng)
+VALUES ('Anheuser-Busch Inc','700 Schrock Rd','Columbus','43229-1123','6148886644','','','',2,-82.99307,40.102197);
+
+
+INSERT INTO breweries (name, address, city, zipcode, phone_number, description, brewery_logo_url, hours, user_id, lat, lng)
+VALUES ('Antiques on High','714 S High St','Columbus','43206','614-725-2070','','','',2,-82.99774,39.9476);
+
+
+INSERT INTO breweries (name, address, city, zipcode, phone_number, description, brewery_logo_url, hours, user_id, lat, lng)
+VALUES ('Aqueduct Brewing','529 Grant St Ste 106','Akron','44311-1184','3306066583','','','',2,-81.51788487,41.0698532);
+
+
+INSERT INTO breweries (name, address, city, zipcode, phone_number, description, brewery_logo_url, hours, user_id, lat, lng)
+VALUES ('Bad Tom Smith Brewing','4720 Eastern Ave','Cincinnati','45226-1893','5138714677','','','',2,-84.41803086,39.11995755);
+
+
+INSERT INTO breweries (name, address, city, zipcode, phone_number, description, brewery_logo_url, hours, user_id, lat, lng)
+VALUES ('Bad Tom Smith Brewing','1836 W 25th St','Cleveland','44113-3142','','','','',2,-81.7051638,41.4861772);
+
+
+INSERT INTO breweries (name, address, city, zipcode, phone_number, description, brewery_logo_url, hours, user_id, lat, lng)
+VALUES ('Baithouse Brewery','','Sandusky','44870','4193665372','','','',2,'','');
+
+
+INSERT INTO breweries (name, address, city, zipcode, phone_number, description, brewery_logo_url, hours, user_id, lat, lng)
+VALUES ('Bardwell Winery and Brewery','716 N High St','Mount Orab','45154','','','','',2,-83.9209789,39.0419795);
+
+
+INSERT INTO breweries (name, address, city, zipcode, phone_number, description, brewery_logo_url, hours, user_id, lat, lng)
+VALUES ('Barleys Brewing Company (Ale House No. 1)','467 N High St','Columbus','43215-2007','6142282537','','','',2,-83.0027669,39.9719408);
+
+
+INSERT INTO breweries (name, address, city, zipcode, phone_number, description, brewery_logo_url, hours, user_id, lat, lng)
+VALUES ('Bascule Brewery And Public House','1397 Colorado Ave','Lorain','44052-3377','','','','',2,-82.15710792,41.46560548);
+
+
+INSERT INTO breweries (name, address, city, zipcode, phone_number, description, brewery_logo_url, hours, user_id, lat, lng)
+VALUES ('Big Ash Brewing Company','','Cincinnati','45244-3244','5133079688','','','',2,'','');
+
+
