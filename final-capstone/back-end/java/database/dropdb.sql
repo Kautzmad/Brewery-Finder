122a1915450c0094b3ada7b8aed532ab7c1b81aa
BrendanGlancy <bglance68@gmail.com>
2020-12-09 11:50:48 -0500

    dropdb

--- final-capstone/back-end/java/database/dropdb.sql
+++ final-capstone/back-end/java/database/dropdb.sql
@@ -1,8 +1,8 @@
 -- **************************************************************
 -- This script destroys the database and associated users
 -- **************************************************************
-SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'capstone';
-DROP DATABASE capstone;
+SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = 'final_capstone';
+DROP DATABASE final_capstone;
 
 DROP USER capstone_owner;
 DROP USER capstone_appuser;
