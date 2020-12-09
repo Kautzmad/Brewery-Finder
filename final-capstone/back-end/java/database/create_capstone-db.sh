3b3591e71a428ac46cd8c7559f2df37363d92b7c
BrendanGlancy <bglance68@gmail.com>
2020-12-09 12:10:08 -0500

    sh might work

--- final-capstone/back-end/java/database/create_capstone-db.sh
+++ final-capstone/back-end/java/database/create_capstone-db.sh
@@ -4,6 +4,6 @@ psql -U postgres -f "$BASEDIR/dropdb.sql" &&
 createdb -U postgres final_capstone &&
 psql -U postgres -d final_capstone -f "$BASEDIR/tableInit.sql" &&
 psql -U postgres -d final_capstone -f "$BASEDIR/authTableSetup.sql" &&
-psql -U postgres -d final_capstone -f "$BASEDIR/data.sql"
-psql -U postgres -d final_capstone -f "$BASEDIR/Breweries.sql"
+psql -U postgres -d final_capstone -f "$BASEDIR/data.sql" &&
+psql -U postgres -d final_capstone -f "$BASEDIR/Breweries.sql" &&
 psql -U postgres -d final_capstone -f "$BASEDIR/beers.sql"
