37294bc379113806135ffb2d88004a812a6b084c
BrendanGlancy <bglance68@gmail.com>
2020-12-09 19:05:10 -0500

    working img

--- final-capstone/front-end/vue/src/components/BeerCard.vue
+++ final-capstone/front-end/vue/src/components/BeerCard.vue
@@ -1,11 +1,20 @@
 <template>
-<div id="card">
+<div id="card" @mouseover="showBeer = true" @mouseleave="showBeer = false">
+    <div v-if="showBeer == false">
+        <img src="../img/QuestionMark.png" width="33%" height="33%">
+    </div>
+    <div class="middle" v-else-if="showBeer != false">
+        <img v-if="this.beer.imgUrl == 'no img'" src="../img/404BeerNotFoundCrop.jpg" alt="Avatar" class="image" >
+        <img v-if="this.beer.imgUrl != 'no img'" v-bind:src="this.beer.imgUrl"  alt="Avatar" class="image">
+    </div>
     <router-link v-bind:to="{ name: 'beer-details', params:{id: this.beer.id} }">
         <h4 class="brewery-name" > {{this.brewery.name}} </h4>
         <h3>{{this.beer.name}} - <span class="beer-type"> {{this.beer.type}} </span></h3>
         <h5> {{this.beer.abv}} ABV </h5>
         <h4 class="beer-info"> {{this.beer.info}} </h4>
     </router-link>
+    </div>
+    
 </div>
 </template>
 <script>
@@ -14,6 +23,7 @@ export default {
     props: ["beer"], 
     data() {
         return { 
+            showBeer: false,
             brewery: {
                 id: 0,
                 name: '',
@@ -56,6 +66,26 @@ a {
 }
 a:hover {
     background-color: gray;
+}
+
+.container {
+  position: relative;
+  width: 50%;
+}
+
+.image {
+  width: 33%;
+  height: 33%;
+  align-items: center;
+}
+
+.overlay {
+  transition: 1s ease;
+  background-color: #008CBA;
+}
+
+.container:hover .overlay {
+  opacity: 1;
 }
 
 
