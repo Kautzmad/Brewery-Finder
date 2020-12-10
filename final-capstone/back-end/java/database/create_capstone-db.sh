8f74dae4a5c73b2891b954c12d69b6f1ea133430
BrendanGlancy <bglance68@gmail.com>
2020-12-10 12:05:14 -0500

    reviews.sql

--- final-capstone/back-end/java/database/create_capstone-db.sh
+++ final-capstone/back-end/java/database/create_capstone-db.sh
@@ -4,4 +4,5 @@ psql -U postgres -f "$BASEDIR/dropdb.sql" &&
 createdb -U postgres final_capstone &&
 psql -U postgres -d final_capstone -f "$BASEDIR/authTableSetup.sql" &&
 psql -U postgres -d final_capstone -f "$BASEDIR/tabelInit.sql" &&
-psql -U postgres -d final_capstone -f "$BASEDIR/data.sql" 
+psql -U postgres -d final_capstone -f "$BASEDIR/data.sql" &&
+psql -U postgres -d final_capstone -f "$BASEDIR/reviews.sql"
