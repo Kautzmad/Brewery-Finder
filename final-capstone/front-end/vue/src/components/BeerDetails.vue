8f59c33730ea43f22f78e59d47acc8cd0f833544
BrendanGlancy <bglance68@gmail.com>
2020-12-14 12:00:08 -0500

    form

--- final-capstone/front-end/vue/src/components/BeerDetails.vue
+++ final-capstone/front-end/vue/src/components/BeerDetails.vue
@@ -1,23 +1,44 @@
 <template>
 <div id="background">
     <div id="details">
-        <h1>{{beer.name}}</h1>
-        <img class="beer-logo" v-bind:src="this.beer.imgUrl"/>
-        <img class="beer-logo-404" v-if="beer.imgUrl == 'no img'" v-bind:src="this.brewery.breweryLogoUrl"/>
-        <h3>{{beer.type}}<span class="beer-ibu" v-if="beer.ibu != 0" > - {{beer.ibu}} IBU</span> - {{beer.abv}} ABV</h3>
-        <h3> {{brewery.website_url}}</h3>
-        <h2 class="beer-desc" v-if="beer.info != 'No description'">{{beer.info}}</h2>
-        <h2 class="beer-desc" v-if="beer.info == 'No description'">{{noDescReplacement}}</h2>
-        <div id="brewery-info">
-            <h1>Brewed by {{brewery.name}}</h1>
-                <router-link v-bind:to="{ name: 'brewery-details', params:{id: beer.breweryId} }">
-            <img class="company-logo" v-bind:src="this.brewery.breweryLogoUrl"/>  
-                </router-link>
-                <!-- here is where we want to include an option to add a review, including review and rating -->
-            <review/>    
+        <div class="heading">
+            <h1 class="brewery-name">{{brewery.name}}</h1>
+
+            <div class="address">
+                <h2><span v-if="brewery.address" >{{brewery.address}}, </span>{{brewery.city}}</h2>
+            </div>
+
+            <div class="website">
+                <a v-bind:href="brewery.websiteUrl" target="_blank"><h3> {{brewery.websiteUrl}}</h3> </a>
+            </div>  
+
+            <img class="company-logo" v-bind:src="this.brewery.breweryLogoUrl"/>
+
+            <div class="description">
+                <h2 class="brewery-desc">{{brewery.description}}</h2>
+                <h2 class="brewery-desc" v-if="!brewery.description">{{noDescReplacement}}</h2>
+            </div>
+        </div>
+
+        <div>
+            <button type="submit" class="addBtn" onclick="window.location.reload();"
+            v-on:click.prevent="addNewBeer(beer)" >Add Beer</button>
+        </div>
+        <div>
+            <button type="submit" class="updateBtn" onclick="window.location.reload();"
+            v-on:click.prevent="updateBeer(beer)" >Update Beer</button>
+        </div>
+        
+        <div class="subheading">
+            <h3>Beers from {{brewery.name}}</h3>
+        </div>
+
+        <div id="beer-list">
+            <beer-card class="card-space" v-for="beer in beers" v-bind:key="beer.name" v-bind:beer="beer"/>
+        </div>
     </div>
-    </div>
-    <div>
+    <div class="newBeerForm">
+         <h1 class="brewery-name">New Beer Form</h1>
     <form>
         <label for="beerName">Name</label>
         <input type="text" id="beerName" name="firstname" placeholder="Name of the beer..">
@@ -50,73 +71,161 @@
 </template>
 <script>
 import applicationServices from '../services/ApplicationServices'
-import Review from './Review.vue'
-
+import BeerCard from './BeerCard'
 export default {
-  components: { Review },
     name: "brewery-details",
+    components: {
+        BeerCard
+    },
     data() {
         return {
-            beer: {
-                id: 0,
-                name: '',
-                abv: 0,
-                ibu: 0,
-                type: '',
-                info: '',
-                imgUrl: '',
-                breweryId: 0,
-                active: true,
-            },
             brewery: {
                 id: 0,
                 name: '',
                 description: '',
                 breweryLogoUrl: '',
-                website_url: ''
+                websiteUrl: ''
             },
-            noDescReplacement: "We can't find a description of this beer, but we're sure it's really great!"
+            beers: [],
+            noDescReplacement: "We can't find a description of this brewery, but we're sure they're really great!"
 
         } 
     },
+    methods: {
+        addNewBeer(){
+            applicationServices.addNewBeer(this.newBeer).then(response=>{
+            if(response.status === 201){
+            alert("Beer successfully added");
+            }  
+            })
+        }
+    },
+    updateBeer(beer){
+            applicationServices.updateBeer(beer).then(response=>{
+            if(response.status === 201){
+            alert("Beer successfully updated");
+            }  
+            })
+        },
     created() {
-        applicationServices.getBeerByID(this.$route.params.id).then(response => {
-            this.beer = response.data
-        }).then(breweryData => {
-            applicationServices.getBreweryByID(this.beer.breweryId).then(response => {
+        applicationServices.getBreweryByID(this.$route.params.id).then(response => {
             this.brewery = response.data
-            console.log(breweryData)
         })
-    })
-    }   
+        applicationServices.getBeerByBreweryID(this.$route.params.id).then(response => {
+            this.beers = response.data
+        })
+    }
 }
 </script>
 <style scoped>
-div#details {
+#details {
+  padding: 0;
+  margin-bottom: -10vh;
+  box-sizing: border-box;
+  
+}
+
+.heading {
+    display: grid;
+    grid-template-columns: auto auto;
     margin-top: 100px;
-    text-align: center;
-    background-color: white;
-    margin-left: 65px; margin-right: 65px;
-    padding-top: 15px; padding-bottom: 15px; padding-left: 150px; padding-right: 150px;
-    border-radius: 20px;
+    grid-column-gap: 1vw;
+    background-color: #C0C0C0;
+    margin: 15vh 20vw 0 20vw;
+    border-radius: 15px;
     box-shadow: inset 0px 0px 10px #000;
 }
 
-img.company-logo, img.beer-logo, img.beer-logo-404 {
-    background-color: rgb(209, 209, 209);
-    border-radius: 5%;
-    width: 200px;
+.submitBtn {
+    text-align: center;
+    margin: 15px;
+}
+
+.subheading {
+    color: 	#404040;
+    font-size: 1.8rem;
+    text-align: center;
+    background-color: whitesmoke;
+    background-position: center;
+    margin: 8% 28% 5% 28%;
+    border-radius: 15px;
     box-shadow: inset 0px 0px 10px #000;
 }
+
+img.company-logo {
+    grid-column: 1/2;
+    grid-row: 1/2;
+    background-color: rgb(209, 209, 209);
+    border-radius: 15px;
+    max-width: 300px;
+    justify-self: center;
+    align-self: center;
+    margin-top: 2vh;
+}
+
+.brewery-name {
+    grid-column: 2/2;
+    font-size: 3rem;
+    grid-row: 1/1;
+    
+}
+
+.website {
+    grid-column: 2/2;
+    grid-row: 2/2;
+    margin-top: -1vh;
+}
+
+.address {
+    grid-column: 2/2;
+    grid-row: 2/2;
+    font-size: 12px;
+    color: black;
+    margin-top: -5vh;
+}
+
+a:link {
+    text-decoration: none;
+    color: black;
+    font-size: 12px;
+}
+
+a:hover {
+    color: 	#696969;
+}
+
+.description {
+    grid-column: 1/3;
+    grid-row: 3/3;
+    text-align: center;
+    margin: 2vh 2vw 0 2vw;
+}
+
 span.beer-type {
     color:rgb(65, 65, 65);
 }
-div#beer-info {
+
+#beer-list {
     padding-top: 15px;
     text-align: center;
 }
-h2.beer-desc {
-    align-items: center;
+
+.card-space {
+    border-radius: 15px;
+    margin-bottom: 10vh;
+    box-shadow: inset 0px 0px 10px #000;
+}
+
+.card-space:nth-child(odd) {
+    background-color: 	#C0C0C0;
+}
+
+.card-space:nth-child(even) {
+    background-color: 	#F5F5F5;
+}
+
+.card-space:hover {
+    background-color: rgb(139, 139, 139);
 }
 
 input[type=text], select {
@@ -144,10 +253,14 @@ input[type=submit]:hover {
   background-color: #45a049;
 }
 
-div {
+.newBeerForm {
   border-radius: 5px;
   background-color: #f2f2f2;
   padding: 20px;
+}
+
+.newBeerForm h1 {
+    text-align: center;
 }
 
 </style>
